092c498c23a0fa252dc2705fa0bfde250ae28aa7
t-sajan <sajan@fusemachines.com>
2021-03-27 18:53:30 +0545

    seperated login test file

---
+++ cypress/integration/daraz_login.spec.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+context("Login to Daraz w/ Email and Password", () => {
+  it("1, 2| Verify that user can login to Daraz", () => {
+    //Go to Daraz Website
+    cy.visit("/");
+
+    //Click Login Option
+    cy.get("#anonLogin").should("be.visible").click();
+    //Assertion to verify user is in login page
+    cy.get(".login-title").should(
+      "contain.text",
+      "Welcome to Daraz! Please login."
+    );
+
+    //Entering Login Email and Password
+    cy.get(".mod-input-loginName").type("jacksnow00700@gmail.com");
+    cy.get(".mod-input-password").type("jackthetester@123");
+    cy.get(".mod-login-btn").should("be.visible").click();
+    cy.wait(2000);
+    //Assertion to verify user is logged in
+    cy.get("#myAccountTrigger").should("have.text", "Jack's account").click();
+
+    //Logout
+    cy.get('a[href*="//member.daraz.com.np/user/logout"]').click({
+      force: true,
+    });
+    cy.get("#anonLogin").should("be.visible");
+  });
+});
